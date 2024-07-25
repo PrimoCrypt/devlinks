30303f347c37fff4817c72108964460805a4343a
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-25 13:01:27 +0100

    Fix: Removed transparent background from the input field and made devlinks color darker

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,7 +6,7 @@ const Links = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-col w-full md:w-auto p-8 md:p-0 md:justify-center md:items-center gap-[64px] md:gap-[51px] h-[100vh] bg-white md:bg-inherit">
         <div className="flex items-center">
           <Image width={40} height={40} src="/logo.svg" alt="" />
-          <h1 className="text-[26.25px] font-bold">devlinks</h1>
+          <h1 className="text-[26.25px] font-bold text-grey-dark">devlinks</h1>
         </div>
         <div>{children}</div>
       </div>
