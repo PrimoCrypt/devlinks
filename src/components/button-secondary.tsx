b4392570f5fa7c6ac73ffc9cd76350ab419e2a05
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-25 14:00:08 +0100

    Feat: Made a responsive navbar

--- src/components/button-secondary.tsx
+++ src/components/button-secondary.tsx
@@ -1,9 +1,20 @@
 import { ButtonType } from "@/types/ButtonTypes";
-
+import { Eye } from "lucide-react";
 const ButtonSecondary = ({ text }: ButtonType) => {
   return (
     <button className="w-full h-[2.875rem] cursor-pointer py-[0.687rem] px-[1.687rem] rounded-lg active:bg-primary-disabled disabled:opacity-[0.25] flex flex-col justify-center items-center border-box text-[1rem] text-primary-default font-semibold border-[1px] border-solid border-primary-default">
-      <div>{text}</div>
+      <div>
+        {text == "preview" ? (
+          <div>
+            <span className="inline md:hidden w-3 h-3">
+              <Eye />
+            </span>
+            <span className="hidden md:inline">{text}</span>
+          </div>
+        ) : (
+          text
+        )}
+      </div>
     </button>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,23 +7,25 @@ import Link from "next/link";
 const NavBar = ({ type }: { type?: string }) => {
   return (
     <div>
-      <div className="w-full relative rounded-xl  bg-white overflow-hidden flex flex-col items-start justify-start py-[1rem] pr-[1rem] pl-[1.5rem] box-border text-left text-[1rem]">
-        <div className="self-stretch flex flex-row items-center justify-between">
-          <div className="w-[9.125rem] gap-[0.375rem] h-[2rem] flex items-center">
+      <div className="w-full relative rounded-xl  bg-white overflow-hidden flex flex-col items-start justify-center py-[1rem] pr-[1rem] pl-[1.5rem] box-border text-left text-[1rem]">
+        <div className="max-w-[85%] md:max-w-full w-full flex flex-row items-center justify-between">
+          <div className=" gap-[0.375rem] h-[2rem] flex items-center">
             <Image width={32} height={32} alt="" src="/logo.svg" />
-            <h1 className="text-[1.313rem] font-bold">devlinks</h1>
+            <h1 className="text-[1.313rem] font-bold md:block hidden">
+              devlinks
+            </h1>
           </div>
-          <div className="flex flex-row items-start justify-start gap-[1rem]">
+          <div className="flex flex-row items-start justify-center gap-4">
             <Link href="/home/links">
               <div
                 className={`rounded-lg  cursor-pointer ${
                   type === "links"
                     ? "bg-primary-disabled text-primary-default"
                     : "bg-transparent text-grey-dark"
-                }  flex flex-row items-center justify-start py-[0.687rem] px-[1.687rem] gap-[0.5rem]`}
+                }  flex flex-row items-center justify-start md:py-[0.687rem] p-2 md:px-[1.687rem] gap-[0.5rem]`}
               >
                 <LinkIcon />
-                <div className="relative leading-[150%] font-semibold">
+                <div className="relative leading-[150%] font-semibold md:block hidden">
                   Links
                 </div>
               </div>
@@ -34,17 +36,20 @@ const NavBar = ({ type }: { type?: string }) => {
                   type === "profile"
                     ? "bg-primary-disabled text-primary-default"
                     : "bg-transparent text-grey-dark"
-                }  flex flex-row items-center justify-start py-[0.687rem] px-[1.687rem] gap-[0.5rem]`}
+                }  flex flex-row items-center justify-start md:py-[0.687rem] p-2 md:px-[1.687rem] gap-[0.5rem]`}
               >
                 <CircleUserRound />
-                <div className="relative leading-[150%] font-semibold">
+                <div className="relative leading-[150%] font-semibold md:block hidden">
                   Profile Details
                 </div>
               </div>
             </Link>
           </div>
-          <div className=" rounded-lg flex flex-col items-start justify-start">
+          <div className="hidden md:flex rounded-lg flex-col items-start justify-start">
             <ButtonSecondary text="Preview" />
+          </div>
+          <div className="rounded-lg flex max-w-6 md:hidden flex-col items-start justify-start">
+            <ButtonSecondary text="preview" />
           </div>
         </div>
       </div>
