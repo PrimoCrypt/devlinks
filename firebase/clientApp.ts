2766262a53d1b935d6b02c6e26ccb0fe295a8f4b
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-25 07:59:10 +0100

    Update: Added a dark background to the homepage

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,6 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/firestore"
+import "firebase/firestore";
+
+const firebaseIn = firebase;
 
 const clientCredenetials = {
   apikey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -8,11 +10,11 @@ const clientCredenetials = {
   projectid: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appid: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appid: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-if(!firebase.getApps.length){
-  firebase.initializeApp(clientCredenetials)
+if (firebaseIn.getApps.length === 0) {
+  firebaseIn.initializeApp(clientCredenetials);
 }
 
-export default firebase
+export default firebaseIn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ function HomeDisplay() {
   }, [router]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-grey-medium">
       <LoaderPinwheel />;
     </div>
   );
